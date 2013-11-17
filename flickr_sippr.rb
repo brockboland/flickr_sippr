e7e7df32d893328013898df22ee05be23e948384
Brock Boland <brock@brockboland.com>
2013-11-17 15:24:37 -0600

    Pull images into an images directory (ignored by git), and added some comments

--- flickr_sippr.rb
+++ flickr_sippr.rb
@@ -1,23 +1,41 @@
 require 'nokogiri'
 require 'uri'
 require 'open-uri'
+require 'fileutils'
 
+# Get the set URL from the path
 set_url = ARGV[0]
 
+if (!set_url)
+  puts "Call this script with a set URL"
+  abort()
+end
+
+if (!File.directory?('images'))
+  FileUtils.mkdir_p('images')
+end
+
+
+# Find the page listing all images in the set
 set_page = Nokogiri::HTML(open(set_url))
 
+# Loop over each photo in the set
 set_page.css('//a.photo-click').each do |link|
   image_url = "http://flickr.com/" + link.attribute('href').to_s 
 
+  # Load the full page for the individual image
   image_page = ''
   open(image_url) do |f|
     image_page = f.read
   end
-  
+
+  # Find the "Original" size image URL somewhere in the page JS
   location = (image_page =~ /http:\/\/farm.*\.staticflickr\.com\/.*_o.jpg/)
   if (location)
+    # Grab a chunk of the JS and strip off where the "[end]" token starts
     roughly = image_page[location, 80]
     real_image_url = roughly[0, roughly.index('[')]
-    puts real_image_url
+    # Pull down the image
+    system("wget -P images " + real_image_url)
   end
 end
